c20d815c17afaf255a71e1c4a2791133b963249c
Pamela Albornoz <p.albornozl@pucp.edu.pe>
2023-05-30 21:25:24 -0500

    Segundo commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,6 +14,9 @@ public class Test {
     /**
      * @param args the command line arguments
      */
+    
+    private String prueba1;
+            
     public static void main(String[] args) {
         System.out.println("Hola");
     }
